ca00f9810b89f8ca51ef3efe72f8e048a57cc675
hayyazk <130911777+hayyazk@users.noreply.github.com>
2024-11-04 07:35:38 +0700

    feat: stochastic hc

---
+++ src/algorithms/hill_climbing/stochastic.py
@@ -0,0 +1,23 @@
+from src.algorithms.base_algorithm import BaseAlgorithm
+from src.utils.data_processing import evaluate_cube, random_swap
+import time
+
+class Stochastic(BaseAlgorithm):
+    def run(self):
+        self.start_time = time.time()
+        self.iter = 0
+        while self.iter < self.max_iter:
+            c_val = self.evaluate()
+            self.tracker.append(c_val)
+
+            neighbor = random_swap(self.cube)
+            n_val = evaluate_cube(neighbor)
+
+            if c_val < n_val:
+                self.cube = neighbor
+            if n_val == 0:
+                break
+
+            self.iter += 1
+        self.end_time = time.time()
+        
